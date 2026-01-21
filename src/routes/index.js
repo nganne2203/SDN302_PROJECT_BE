c4c45751ead85437e56264fe7060d7bfb44a45eb
nganne <thanhngan.pt2004@gmail.com>
2026-01-21 10:00:46 +0700

    implement user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { AUTH_ROUTE } from '#routes/authRoute.js'
+import { BRANCH_ROUTE } from '#routes/branchRoute.js'
+import { USER_ROUTE } from '#routes/userRoute.js'
 
 const Router = express.Router()
 
@@ -8,5 +10,6 @@ Router.get('/', (req, res) => {
   res.send('Welcome to the Phone Accessories API')
 })
 Router.use('/api/auth', AUTH_ROUTE)
-
+Router.use('/api/users', USER_ROUTE)
+Router.use('/api/branch', BRANCH_ROUTE)
 export const ROUTES = Router
